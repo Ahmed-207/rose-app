cb78c363e70d6139e5dc8795c74072d5c65118f4
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-06-15 14:16:07 +0300

    preparing routing , auth lib integration with login - deletion of user-data lib needed -

--- apps/roseAuth/src/app/core/layout/auth-layout/auth-layout.ts
+++ apps/roseAuth/src/app/core/layout/auth-layout/auth-layout.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
-import { Login } from "../../../pages/login/login";
 import { UiLangSwitcher } from "@org/ui-lang-switcher";
 
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet, Login, UiLangSwitcher],
+  imports: [RouterOutlet, UiLangSwitcher],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
 })
 export class AuthLayout {
-  
+
 }
+
