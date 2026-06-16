7710b2197140704b8674208cd6c93c896448f501
Ahmed Atef <adamatef2023-cmd>
2026-06-16 19:52:54 +0300

    creae footer commponent and fixed navbar translation

--- apps/roseMain/src/app/core/layout/Main layout/mainLayout.ts
+++ apps/roseMain/src/app/core/layout/Main layout/mainLayout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../Navbar/navbar';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "../Footer/footer";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [CommonModule ,RouterOutlet ,Navbar ],
+  imports: [CommonModule, RouterOutlet, Navbar, Footer],
   templateUrl: './mainLayout.html',
   styleUrl: './mainLayout.css',
 })
