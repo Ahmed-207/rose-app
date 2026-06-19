58b4e9a7e1624925c418549da6b74f6cbf7dd05b
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-06-19 03:40:07 +0300

    adding routing configs for main/home ,, removing welcome message and applying main-layout with navbar/secondary navbar based on login case, adding logic for shared auth service to access user status/role access rights , adding routing configs for registration and login in entry.routes.ts / app.routes.ts for shell, handling the remaining unhandled parts of translation and editing on page structure for main-layout , navbar/secondary navbar and footer

--- apps/roseMain/src/app/core/layout/secondry Navbar/secondryNavbar.ts
+++ apps/roseMain/src/app/core/layout/secondry Navbar/secondryNavbar.ts
@@ -1,33 +1,22 @@
-// import { Component } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { MenuItem, MessageService } from 'primeng/api';
-
 import { CommonModule } from '@angular/common';
-import { Component ,inject, OnInit} from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
-// import { Translate } from '../../services/Translate/translate';
-import  { LangService } from '@org/ui-lang-switcher';
+import { LangService, UiLangSwitcher } from '@org/ui-lang-switcher';
 
 @Component({
-  template: `
-        <div class="text-right">
-                    <p class="text-xs text-gray-400 dark:text-gray-500 leading-tight">Hello</p>
-                    <p class="text-sm font-bold leading-tight flex items-center gap-1">
-                        user
-                        <i class="fa-solid fa-chevron-down text-[10px] text-gray-400"></i>
-                    </p>
-                </div>
-    `,
-     providers: [MessageService],
-  selector: 'app-secondry-navbar',
-  imports: [MenuModule,ButtonModule , CommonModule, TranslatePipe],
-  templateUrl: './secondryNavbar.html',
-  styleUrl: './secondryNavbar.css',
+    providers: [MessageService],
+    selector: 'app-secondry-navbar',
+    imports: [MenuModule, ButtonModule, CommonModule, TranslatePipe, UiLangSwitcher],
+    templateUrl: './secondryNavbar.html',
+    styleUrl: './secondryNavbar.css',
 })
 export class SecondryNavbar implements OnInit {
-  mytranclateService:LangService =inject(LangService);
-   private messageService = inject(MessageService);
+    mytranclateService: LangService = inject(LangService);
+    userName = input<string | undefined>('')
+    private messageService = inject(MessageService);
     items: MenuItem[] | undefined;
 
     ngOnInit() {
@@ -39,7 +28,7 @@ export class SecondryNavbar implements OnInit {
                         label: 'Account',
                         icon: 'pi pi-user'
                     },
-                     {
+                    {
                         label: 'Adress',
                         icon: 'pi pi-plus'
                     },
@@ -61,7 +50,3 @@ export class SecondryNavbar implements OnInit {
     }
 }
 
-// export class SecondryNavbar  {
-//   mytranclateService:Translate =inject(Translate);
-  
-// }
