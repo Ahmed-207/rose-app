05139bde98b16e4000e8149bd98006f062e57abb
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-06-20 14:14:28 +0300

    adding the toggler to auth , fixing issue with language change and direction of toggle by adding effect in component constructor that depend on the current language value

--- apps/roseAuth/src/app/core/layout/auth-layout/auth-layout.ts
+++ apps/roseAuth/src/app/core/layout/auth-layout/auth-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { UiLangSwitcher } from "@org/ui-lang-switcher";
+import { ThemeToggler } from "@org/shared-theme";
 
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet, UiLangSwitcher],
+  imports: [RouterOutlet, UiLangSwitcher, ThemeToggler],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
 })
