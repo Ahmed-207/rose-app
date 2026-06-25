831cd7bff145be6373a5d48a93dcbd01c2fe9af7
ManarYousry <manar.yousry@soultech.com>
2026-06-25 23:25:19 +0300

    inprogress

--- apps/roseMain/src/app/features/home/pages/homePage.ts
+++ apps/roseMain/src/app/features/home/pages/homePage.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SpecialGiftSection } from '../components/specialGiftSection';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [SpecialGiftSection],
   templateUrl: './homePage.html',
   styleUrl: './homePage.css',
 })
