1e7545a43674e267221f2ad122135d67ca7b34c6
ManarYousry <manar.yousry@soultech.com>
2026-06-28 13:52:05 +0300

    finish special gift section

--- apps/roseMain/src/app/features/home/components/specialGiftSection.ts
+++ apps/roseMain/src/app/features/home/components/specialGiftSection.ts
@@ -4,23 +4,13 @@ import { CommonModule } from '@angular/common';
 import { GalleriaModule } from 'primeng/galleria';
 import { ButtonModule } from 'primeng/button';
 import { BadgeModule } from 'primeng/badge';
+import { TranslatePipe } from '@ngx-translate/core';
+import { CategoryCard, HeroSlide } from '../models/special-gift-Dtos';
 
-export interface HeroSlide {
-  title: string;
-  subtitle: string;
-  btnLabel: string;
-  imageUrl: string;
-}
 
-export interface CategoryCard {
-  tag: string;
-  title: string;
-  bgGradient: string;
-  imagUrl: string;
-}
 @Component({
   selector: 'special-gift-section',
- imports: [CommonModule, GalleriaModule, ButtonModule, BadgeModule],
+ imports: [CommonModule, GalleriaModule, ButtonModule, BadgeModule,TranslatePipe],
   templateUrl: './specialGiftSection.html',
   styleUrl: './specialGiftSection.css',
 })
@@ -36,20 +26,20 @@ export class SpecialGiftSection {
       title: 'Say It with Flowers',
       subtitle: 'Elegant gifts for every special moment.',
       btnLabel: "I'm buying!",
-      imageUrl: '../assets/images/img1.png',
+      imageUrl: '/assets/images/img1.png',
     },
     {
       title: 'Indulge in Sweet Love',
       subtitle: 'Artisan chocolates crafted with care.',
       btnLabel: 'Explore Now',
-      imageUrl: 'assets/images/img2.png',
+      imageUrl: '/assets/images/img2.png',
 
     },
     {
       title: 'Celebrate Every Milestone',
       subtitle: 'Curated sets for anniversaries & weddings.',
       btnLabel: 'Browse Sets',
-      imageUrl: 'assets/images/img3.png',
+      imageUrl: '/assets/images/img3.png',
     },
   ];
 
@@ -59,19 +49,19 @@ export class SpecialGiftSection {
       tag: 'Wedding',
       title: "Celebrate Her Forever with a Gift She’ll Always Remember",
       bgGradient: 'linear-gradient(135deg,#6b4c2a 0%,#c49a50 100%)',
-      imagUrl: '../assets/images/img3.png',
+      imagUrl: '/assets/images/img3.png',
     },
     {
       tag: 'Engagement',
       title: 'Honor the Beginning of a Beautiful Journey Together',
       bgGradient: 'linear-gradient(135deg,#d4c5a0 0%,#b8976a 100%)',
-      imagUrl: '../../assets/images/img3.png',
+      imagUrl: '/assets/images/img3.png',
     },
     {
       tag: 'Anniversary',
       title: 'Mark Every Year of Love with a Meaningful Surprise',
       bgGradient: 'linear-gradient(135deg,#7a0f1e 0%,#c03040 100%)',
-      imagUrl: '../../../../assets/images/img3.png',
+      imagUrl: '/assets/images/img3.png',
     },
   ];
 }
