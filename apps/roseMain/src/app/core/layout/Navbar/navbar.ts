98444fa86e2d48f4d3e3b43ca57dcc2d6e67a770
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-07-05 09:39:57 +0300

    fixing navbar in responsive , fixing secondary navbar in responsive , updating category filter in most popular with using the reusable section - header , using category filter button in small screens - fixing theming to be identical with figma for pagination

--- apps/roseMain/src/app/core/layout/Navbar/navbar.ts
+++ apps/roseMain/src/app/core/layout/Navbar/navbar.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { UiLangSwitcher } from '@org/ui-lang-switcher';
 import { ThemeToggler } from "@org/shared-theme";
-
 
 
 @Component({
@@ -16,10 +15,17 @@ import { ThemeToggler } from "@org/shared-theme";
 export class Navbar {
   private readonly router = inject(Router);
 
-
   goToLogin(): void {
     this.router.navigateByUrl('/auth/login');
   }
 
+  isMobileMenuOpen = signal(false);
 
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen.update(v => !v);
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen.set(false);
+  }
 }
