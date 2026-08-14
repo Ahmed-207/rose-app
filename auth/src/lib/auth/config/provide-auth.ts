97789663fad574d60f5f07bde2eaba5cec18792e
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-08-14 18:07:48 +0300

    feat(auth): allow extra interceptors and withFetch in provideAuth

--- auth/src/lib/auth/config/provide-auth.ts
+++ auth/src/lib/auth/config/provide-auth.ts
@@ -2,18 +2,31 @@ import {
   EnvironmentProviders,
   makeEnvironmentProviders,
 } from '@angular/core';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { authInterceptor } from '../interceptors/auth.interceptor';
 import { errorInterceptor } from '../interceptors/error.interceptor';
 import { API_URL } from './api';
 
 export interface AuthConfig {
   apiUrl: string;
+  extraInterceptors?: HttpInterceptorFn[];
 }
 
 export function provideAuth(config: AuthConfig): EnvironmentProviders {
   return makeEnvironmentProviders([
     { provide: API_URL, useValue: config.apiUrl },
-    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        authInterceptor,
+        errorInterceptor,
+        ...(config.extraInterceptors ?? []),
+      ]),
+    ),
   ]);
 }
